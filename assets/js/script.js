f5f16721da47b63b668101a2d56ebba9680d109e
Woll Dog <110208272+wolldog@users.noreply.github.com>
2022-12-10 21:30:00 +1100

    Add variables to create elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 var titleDiv = document.querySelector("#titleDiv")
 var contentDiv = document.querySelector("#contentDiv")
 var resultDiv = document.querySelector("#resultdiv")
+
+//variables to create elements in document
+var h1El = document.createElement("h1");
+var pEl = document.createElement("p");
+var labelEl = document.createElement("label")
+var inputEl = document.createElement("input")
+var formEl = document.createElement("form")
+var buttonEl = document.createElement("button")
