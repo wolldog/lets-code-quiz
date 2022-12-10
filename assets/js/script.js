009e18f3808301ee0b264669ea29ad5454aad207
Woll Dog <110208272+wolldog@users.noreply.github.com>
2022-12-10 21:33:03 +1100

    Add questions object

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,37 @@ var labelEl = document.createElement("label")
 var inputEl = document.createElement("input")
 var formEl = document.createElement("form")
 var buttonEl = document.createElement("button")
+
+//create questions object
+
+var questions = [
+    {
+    question : "Arrays in JavaScript can be used to store ______ .",
+    choices : ["numbers and strings", 'other arrays', 'booleans', 'all of the above'],
+    correctAnswer : "all of the above"
+    },
+
+    {
+    question : "String values must be enclosed within ______ when being assigned to variables.",
+    choices : ["commas", 'curly brackets', 'quotes', 'parentheses'],
+    correctAnswer : "quotes"
+    },
+
+    {
+    question : "The condition in an if/else statement is enclosed within ______. ",
+    choices : ["quotes", 'curly brackets', 'parentheses', 'square brackets'],
+    correctAnswer : "parentheses"
+    },
+
+    {
+    question : "Javascript is an __________ language.",
+    choices : ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
+    correctAnswer : "Object-Oriented"
+    },
+
+    {
+    question : "What keyword is used to check whether a given property is valid or not?",
+    choices : ["in", 'is in', 'exists', 'lies'],
+    correctAnswer : "in"
+    }
+]
