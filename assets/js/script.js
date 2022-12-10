22896566ffdee62d59c02db55ed9c4d270b5798d
Woll Dog <110208272+wolldog@users.noreply.github.com>
2022-12-10 21:26:19 +1100

    Add document location variables

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//variables to reference locations in docmument
+var titleDiv = document.querySelector("#titleDiv")
+var contentDiv = document.querySelector("#contentDiv")
+var resultDiv = document.querySelector("#resultdiv")
