c9c23e127c8b0d9dd04c390df446ecc2d4466176
Woll Dog <110208272+wolldog@users.noreply.github.com>
2022-12-11 23:05:07 +1100

    Bug fix - Clear score duplicates scores

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,15 +5,17 @@ var scoreTable = document.querySelector("#scoreBoardTbl")
 var goBackBtn = document.querySelector("#back")
 var clearScoreBtn = document.querySelector("#clearScores")
 
-var initials = document.querySelector
 
-
-//variables to create elements in document
+//variables to create list elements in document
 var liEl = document.createElement("li");
+
+//Retrieve initials and scores from local storage
 
 var storedScores = JSON.parse(localStorage.getItem("scoreBoard"))
 
 function init(){
+
+    // Hide no stored score placeholder if scores are available
 
     if (storedScores !== null) {
 
@@ -21,6 +23,7 @@ function init(){
     }
 
 
+    // Create list elements and populate with stored initials and scores
 
     for (var i = 0; i < storedScores.length; i++ ){
         var liEl = document.createElement("li");
@@ -31,8 +34,7 @@ function init(){
         }
 
 
-    console.log(storedScores.length)
-        
+    console.log(storedScores.length)   
     console.log(storedScores)
 
 
@@ -42,8 +44,12 @@ function init(){
 
 clearScoreBtn.addEventListener("click", function () {
     localStorage.clear();
-    init ();
+    location.reload();
+    
+
 });
+
+// Navigate back to quiz landing page
 
 goBackBtn.addEventListener("click", function() {
     window.location.replace("../../index.html")
